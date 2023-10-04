af87c338b4a54aa85a22b928a93c5ab875275ed4
Tharun8951 <tharunappu2004@gmail.com>
2023-10-04 11:56:30 -0700

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
 app.use(express.json())
+
+app.use(
+    cors({
+      origin: [
+        'http://localhost:5173/',
+      ],
+    }),
+  )
 
 const mongoDB = require('./db')
 const productsHandler = require('./Routers/Products.router')
